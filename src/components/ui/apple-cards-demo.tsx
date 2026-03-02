d1ac201b3f97eb64838be4dc7e9ebaf18cef390f
shazebkhan22 <shazeb.khan@somaiya.edu>
2026-03-02 11:54:22 +0530

    Added cards carousel

--- src/components/ui/apple-cards-demo.tsx
+++ src/components/ui/apple-cards-demo.tsx
@@ -9,8 +9,8 @@ export function AppleCardsCarouselDemo() {
   ));
 
   return (
-    <div className="w-full h-full py-20">
-      <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+    <div className="w-full h-full">
+      <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-semibold text-neutral-800 font-sans">
         Get to know your iSad.
       </h2>
       <Carousel items={cards} />
